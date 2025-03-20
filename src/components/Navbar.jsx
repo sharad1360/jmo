4a29aa917213be0ee2f5e0a13f98d0c8f8d3a91f
sharad1360 <sharad1360.be22@chitkarauniversity.edu.in>
2025-03-20 11:56:05 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-900 text-white fixed w-full z-50 shadow-md">
       <div className="container mx-auto flex justify-between items-center px-6 py-4">
-        
+
         {/* Logo */}
         <a href="/" className="flex items-center space-x-2">
           <img src="/images/jmo-logo.png" alt="JMO Logo" className="w-12 h-12" />
@@ -23,11 +23,10 @@ const Navbar = () => {
 
         {/* Nav Links */}
         <ul
-          className={`absolute lg:static bg-gray-900 lg:bg-transparent w-full lg:w-auto left-0 top-16 lg:flex items-center space-x-6 lg:space-x-8 p-6 lg:p-0 transition-transform duration-300 ${
-            isOpen ? "translate-y-0" : "-translate-y-[500px] lg:translate-y-0"
-          }`}
+          className={`absolute lg:static bg-gray-900 lg:bg-transparent w-full lg:w-auto left-0 top-16 lg:flex items-center space-x-6 lg:space-x-8 p-6 lg:p-0 transition-transform duration-300 ${isOpen ? "translate-y-0" : "-translate-y-[500px] lg:translate-y-0"
+            }`}
         >
-          {["Home", "Products", "Services", "About", "Contact"].map((item) => (
+          {["Home", "Popular-Product", "Services", "Products", "About", "Contact"].map((item) => (
             <li key={item} className="text-center lg:text-left">
               <a
                 href={`#${item.toLowerCase()}`}
